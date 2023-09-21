b4d40a67ffc6877d77794b1734f943b39021e495
CarlosZ360 <carlos.zarate@ucb.edu.bo>
2023-09-21 19:58:24 -0400

    added structure view profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddStudentComponent } from './components/add-student/add-student.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
 
 const routes: Routes = [
-  { path: 'add-student', component: AddStudentComponent }
+  { path: 'add-student', component: AddStudentComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddStudentComponent } from './components/add-student/add-student.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddStudentComponent
+    AddStudentComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
