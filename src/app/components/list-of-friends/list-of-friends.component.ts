3024807da5b98acffee326a69413596e6530c28f
CarlosZ360 <carlos.zarate@ucb.edu.bo>
2023-10-03 16:05:16 -0400

    details list-of-friends

--- src/app/components/list-of-friends/list-of-friends.component.ts
+++ src/app/components/list-of-friends/list-of-friends.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-of-friends.component.css']
 })
 export class ListOfFriendsComponent {
+  
+  ngOnInit(){
+    this.friends=this.friends
+  }
+
+  friends = [
+    {
+      userId: 1,
+      Nombre: 'Gonzalo',
+      Apellido: 'Choque',
+      Carrera: 'Sistemas'
+    },
+    {
+      userId: 2,
+      Nombre: 'Valeria',
+      Apellido: 'Paredez',
+      Carrera: 'Civil'
+    }
+  ];
+
+  //Logica para el popup
+  isDialogVisible = false;
+
+  openConfirmationDialog() {
+    this.isDialogVisible = true;
+  }
+
+  confirmDelete() {
+    // Realiza la eliminación aquí
+    console.log('Elemento eliminado');
+    this.isDialogVisible = false; // Cierra el cuadro de diálogo
+  }
+
+  cancelDelete() {
+    // Cancela la eliminación aquí
+    console.log('Eliminación cancelada');
+    this.isDialogVisible = false; // Cierra el cuadro de diálogo
+  }
 
 }
